df09831e3342417666406fb419337797d56ed370
arekkab <arekkab@VANNES>
2017-06-14 16:57:13 +0200

    Ajoutez des fichiers projet.

---
+++ AJSWebapi/App_Data/SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO dbo.TVShow
+VALUES ('Game of Thrones',2011,'Peter Dinklage,Nikolaj Coster-Waldau')
+,('The Walking Dead',2010,'Andrew Lincoln, Steven Yeun')
+,('Colony',2016,'Josh Holloway, Sarah Wayne Callies')
+,('Vikings',2013,'Travis Fimmel, Katheryn Winnick')
---
+++ AJSWebapi/App_Data/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿
+CREATE TABLE [dbo].[TVShow](
+[Id] [int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+[Name] [varchar](200) NULL,
+[Year] [int] NULL,
+[Actors] [varchar](300) NULL)
